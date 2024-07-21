80869b81c6d3952fed25c0e6f9c2764062885589
Jasson Rolando Cu <jassoncucaal14@hotmail.com>
2024-07-21 16:41:01 -0600

    feat: :fire: firts commit

---
+++ Hotel.Persistence/Context/ApplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace Clinical.Persistence.Context
+{
+    public class ApplicationDBContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public ApplicationDBContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("HotelConnection")!;
+        }
+
+        public IDbConnection CreateConnection => new SqlConnection(_connectionString);
+    }
+}
