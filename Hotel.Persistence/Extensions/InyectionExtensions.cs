41181ecb3a3fc32267899f313163c4a1c95815af
Jasson Rolando Cu <jassoncucaal14@hotmail.com>
2024-08-01 09:45:33 -0600

    feat: :fire: Add `InyectionExtension` class

---
+++ Hotel.Persistence/Extensions/InyectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Hotel.Persistence.Context;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hotel.Persistence.Extensions
+{
+    public static class InyectionExtensions
+    {
+        public static IServiceCollection AddInyectionPersistence(this IServiceCollection services)
+        {
+            services.AddSingleton<ApplicationDBContext>();
+
+            return services;
+        }
+    }
+
+}
