3fefe82b3aad9e9125be974ed76d2dc81b31918f
Jasson Rolando Cu <jassoncucaal14@hotmail.com>
2024-08-01 16:22:34 -0600

    feat: :ambulance: Add another Store procedures

--- Hotel.Utils/Constants/StoreProcedures.cs
+++ Hotel.Utils/Constants/StoreProcedures.cs
@@ -4,6 +4,10 @@
     {
         #region Employee Store Procedures
         public const string uspEmployeesLis = "uspEmployeesList";
+        public const string uspEmployeeById = "uspEmployeeById";
+        public const string uspEmployeeDelete = "uspEmployeeDelete";
+        public const string uspEmployeeEdit = "uspEmployeeEdit";
+        public const string uspEmployeeInsert = "uspEmployeeInsert";
         #endregion
 
     }
