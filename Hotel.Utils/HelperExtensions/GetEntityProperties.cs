73f9faa4043e15173e28b8e254665d0f6921faaa
Jasson Rolando Cu <jassoncucaal14@hotmail.com>
2024-08-01 16:28:29 -0600

    feat: :chart_with_upwards_trend: Add `GetEntityProperties`

---
+++ Hotel.Utils/HelperExtensions/GetEntityProperties.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+namespace Hotel.Utils.HelperExtensions
+{
+    public static class GetEntityProperties
+    {
+        public static Dictionary<string, object> GetPropertiesWithValues<T>(this T entity)
+        {
+            PropertyInfo[] properties = typeof(T).GetProperties();
+            var entityParams = new Dictionary<string, object>();
+
+            foreach (PropertyInfo property in properties)
+            {
+                object value = property.GetValue(entity)!;
+
+                if (value != null)
+                {
+                    entityParams[property.Name] = value;
+                }
+            }
+
+            return entityParams;
+        }
+    }
+}
