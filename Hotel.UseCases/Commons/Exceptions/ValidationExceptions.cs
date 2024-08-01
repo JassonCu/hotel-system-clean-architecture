374947e457ae61a5fa16def42c01368ecb25d30d
Jasson Rolando Cu <jassoncucaal14@hotmail.com>
2024-08-01 16:02:29 -0600

    feat: :fire: Add Validators

---
+++ Hotel.UseCases/Commons/Exceptions/ValidationExceptions.cs
@@ -0,0 +1,18 @@
+﻿using Hotel.UseCases.Commons.Bases;
+
+namespace Hotel.UseCases.Commons.Exceptions
+{
+    public class ValidationExceptions : Exception
+    {
+        public IEnumerable<BaseError>? Errors { get; }
+        public ValidationExceptions() : base()
+        {
+            Errors = new List<BaseError>();
+        }
+
+        public ValidationExceptions(IEnumerable<BaseError>? errors) : this()
+        {
+            Errors = errors;
+        }
+    }
+}
