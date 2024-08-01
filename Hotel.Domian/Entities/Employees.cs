1f7b71e7d53668d84daec6baa8a93cf5c6885fe4
Jasson Rolando Cu <jassoncucaal14@hotmail.com>
2024-08-01 13:45:07 -0600

    feat: :fire: Add `Employees` entitity

---
+++ Hotel.Domian/Entities/Employees.cs
@@ -0,0 +1,20 @@
+﻿namespace Hotel.Domian.Entities
+{
+    public class Employees
+    {
+        public int EmployeeID { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Position { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public DateTime? HireDate { get; set; }
+        public decimal? Salary { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? PostalCode { get; set; }
+        public string? Country { get; set; }
+    }
+
+}
