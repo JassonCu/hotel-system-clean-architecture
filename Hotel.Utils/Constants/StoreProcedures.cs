438164289039d8655e1458bd4363998156cd9e94
Jasson Rolando Cu <jassoncucaal14@hotmail.com>
2024-08-01 13:52:47 -0600

    feat: :tada: Add `Store procedures`

---
+++ Hotel.Utils/Constants/StoreProcedures.cs
@@ -0,0 +1,11 @@
+﻿namespace Hotel.Utils.Constants
+{
+    public class StoreProcedures
+    {
+        #region Employee Store Procedures
+        public const string uspEmployeesLis = "uspEmployeesList";
+        #endregion
+
+
+    }
+}
