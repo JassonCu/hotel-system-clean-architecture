d74ab095082b8ca0f14167e90b85c41ecd9933b2
Jasson Rolando Cu <jassoncucaal14@hotmail.com>
2024-08-01 16:08:01 -0600

    feat: :rocket: Add `InyectionExtensions`

--- Hotel.Persistence/Extensions/InyectionExtensions.cs
+++ Hotel.Persistence/Extensions/InyectionExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Hotel.Persistence.Context;
+﻿using Hotel.Interface.Interfaces;
+using Hotel.Persistence.Context;
+using Hotel.Persistence.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Hotel.Persistence.Extensions
@@ -8,9 +10,9 @@ namespace Hotel.Persistence.Extensions
         public static IServiceCollection AddInyectionPersistence(this IServiceCollection services)
         {
             services.AddSingleton<ApplicationDBContext>();
+            services.AddScoped<IEmployeesRepository, EmployeesRepositories>();
 
             return services;
         }
     }
-
 }
---
+++ Hotel.UseCases/Extensions/InyectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Hotel.UseCases.Commons.Behaviours;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Hotel.UseCases.Extensions
+{
+    public static class InyectionExtensions
+    {
+        public static IServiceCollection AddInyectionApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(x => x.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssemblies(new List<Assembly> { Assembly.GetExecutingAssembly() });
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviours<,>));
+            
+            return services;
+        }
+    }
+}
