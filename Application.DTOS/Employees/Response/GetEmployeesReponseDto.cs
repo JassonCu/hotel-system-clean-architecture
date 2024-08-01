d5036b94b3db00f2098b0be38ac8c4bad30b1d44
Jasson Rolando Cu <jassoncucaal14@hotmail.com>
2024-08-01 13:54:48 -0600

    feat: :sparkles: Add `GetEmployeesReponseDto`

---
+++ Application.DTOS/Employees/Response/GetEmployeesReponseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Application.DTOS.Employees.Response
+{
+    public class GetEmployeesReponseDto
+    {
+        public int EmployeeID { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Position { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public DateTime? HireDate { get; set; }
+    }
+}
