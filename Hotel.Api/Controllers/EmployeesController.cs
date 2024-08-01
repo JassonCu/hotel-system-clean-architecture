05431190c7fe9e332a7749e47f0f0a3d180a434d
Jasson Rolando Cu <jassoncucaal14@hotmail.com>
2024-08-01 17:28:37 -0600

    feat: :white_check_mark: Add `EmployeeController`

---
+++ Hotel.Api/Controllers/EmployeesController.cs
@@ -0,0 +1,26 @@
+﻿using Hotel.UseCases.Employees.Queries.GetAllQuery;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public EmployeesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ListEmployees()
+        {
+            var response = await _mediator.Send(new GetAllEmployeeQuery());
+
+            return Ok(response);
+        }
+    }
+}
