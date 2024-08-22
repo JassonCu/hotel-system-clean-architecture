e1d69cb83781d476e4dff52d47c41ba6e323bc2d
Jasson Rolando Cu <jassoncucaal14@hotmail.com>
2024-08-22 15:08:58 -0600

    feat: :bug: Variable name error

--- Hotel.Utils/Constants/StoreProcedures.cs
+++ Hotel.Utils/Constants/StoreProcedures.cs
@@ -3,7 +3,7 @@
     public class StoreProcedures
     {
         #region Employee Store Procedures
-        public const string uspEmployeesLis = "uspEmployeesList";
+        public const string uspEmployeesList = "uspEmployeesList";
         public const string uspEmployeeById = "uspEmployeeById";
         public const string uspEmployeeDelete = "uspEmployeeDelete";
         public const string uspEmployeeEdit = "uspEmployeeEdit";
