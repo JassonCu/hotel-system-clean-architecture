5c5ebd8ade9ec496ece93c85b71c0d1a0c849f24
Jasson Rolando Cu <jassoncucaal14@hotmail.com>
2024-08-22 12:34:40 -0600

    feat: :fire: Add connetions string

--- Hotel.Api/Program.cs
+++ Hotel.Api/Program.cs
@@ -1,16 +1,31 @@
-using Hotel.Persistence.Extensions;
+using Hotel.Persistence.Context;
 using Hotel.UseCases.Extensions;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+/*
+La basde datos usada en este proyecto es una base de datos que se hizo
+a partir de un contenedor docker para facilitar el desarrollo de la misma.
+Cuando termine el desarrollo y se suba la última actualización se eliminará el contenedor.
+*/
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("HotelConnectionString"));
+});
+
+var connectionString = builder.Configuration.GetConnectionString("HotelConnectionString");
+Console.WriteLine($"Connection String: {connectionString}");
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddInyectionPersistence();
 builder.Services.AddInyectionApplication();
 
 var app = builder.Build();
