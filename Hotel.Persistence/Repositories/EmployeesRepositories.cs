ff202b1c00a9d25454f43177fa0c4acfbd6f8302
Jasson Rolando Cu <jassoncucaal14@hotmail.com>
2024-08-22 14:21:38 -0600

    fix:

--- Hotel.Persistence/Repositories/EmployeesRepositories.cs
+++ Hotel.Persistence/Repositories/EmployeesRepositories.cs
@@ -2,28 +2,54 @@
 using Hotel.Domian.Entities;
 using Hotel.Interface.Interfaces;
 using Hotel.Persistence.Context;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System.Data;
 
 namespace Hotel.Persistence.Repositories
 {
     public class EmployeesRepositories : IEmployeesRepository
     {
-        private readonly ApplicationDBContext _dbContext;
+        private readonly ApplicationDbContext _dbContext;
+        private readonly ILogger<EmployeesRepositories> _logger;
 
-        public EmployeesRepositories(ApplicationDBContext dbContext)
+
+        public EmployeesRepositories(ApplicationDbContext dbContext, ILogger<EmployeesRepositories> logger)
         {
             _dbContext = dbContext;
+            _logger = logger;
         }
 
-        public async Task<IEnumerable<Employees>> ListEmployees()
+        public async Task<IEnumerable<Employee>> ListEmployees()
         {
-            using var connection = _dbContext.CreateConnection;
+            try
+            {
+                using var connection = _dbContext.Database.GetDbConnection();
 
-            var query = "uspEmployeesList";
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.Open();
+                }
 
-            var employees = await connection.QueryAsync<Employees>(query, commandType: CommandType.StoredProcedure);
+                var query = "uspEmployeesList";
 
-            return employees;
+                var employees = await connection.QueryAsync<Employee>(query, commandType: CommandType.StoredProcedure);
+
+                return employees;
+            }
+            catch (SqlException sqlEx)
+            {
+                // Registro de excepciones específicas de SQL
+                _logger.LogError(sqlEx, "Error al ejecutar el stored procedure 'uspEmployeesList'");
+                throw new ApplicationException("Ocurrió un error al recuperar los empleados. Por favor, intente nuevamente más tarde.");
+            }
+            catch (Exception ex)
+            {
+                // Registro de excepciones generales
+                _logger.LogError(ex, "Error inesperado en el método ListEmployees");
+                throw new ApplicationException("Ocurrió un error inesperado. Por favor, intente nuevamente más tarde.");
+            }
         }
     }
 }
