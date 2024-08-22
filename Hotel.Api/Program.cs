7111397e11ded4e899d796a7eb19534f80810f99
Jasson Rolando Cu <jassoncucaal14@hotmail.com>
2024-08-22 14:32:40 -0600

    feat: :art: Add `ConnectionString` into `Program.cs` and important Message

--- Hotel.Api/Program.cs
+++ Hotel.Api/Program.cs
@@ -6,33 +6,28 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-/*
-La basde datos usada en este proyecto es una base de datos que se hizo
-a partir de un contenedor docker para facilitar el desarrollo de la misma.
-Cuando termine el desarrollo y se suba la última actualización se eliminará el contenedor.
-*/
-
-
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
+builder.Services.AddInyectionPersistence();
+builder.Services.AddInyectionApplication();
 
+/*
+The database used in this project is a database that was made
+from a docker container to facilitate its development.
+When development is finished and the latest update is uploaded, the container will be deleted.
+*/
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("HotelConnectionString"));
 });
 
-builder.Services.AddInyectionPersistence();
-
-
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddInyectionApplication();
 
 var app = builder.Build();
 
--- Hotel.Client/Program.cs
+++ Hotel.Client/Program.cs
@@ -1,9 +1,31 @@
+using Hotel.Persistence.Context;
+using Hotel.Persistence.Extensions;
+using Hotel.UseCases.Extensions;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+builder.Services.AddInyectionApplication();
+builder.Services.AddInyectionPersistence();
+
+/*
+The database used in this project is a database that was made
+from a docker container to facilitate its development.
+When development is finished and the latest update is uploaded, the container will be deleted.
+*/
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("HotelConnectionString"));
+});
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
