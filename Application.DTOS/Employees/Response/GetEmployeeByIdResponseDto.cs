76205192e8a84526fa8de2f0e0959068ba03f343
Jasson Rolando Cu <jassoncucaal14@hotmail.com>
2024-08-28 12:04:36 -0600

    feat: :fire: Add `GetEmployeeById` classes

---
+++ Application.DTOS/Employees/Response/GetEmployeeByIdResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.DTOS.Employees.Response
+{
+    public class GetEmployeeByIdResponseDto
+    {
+        public int EmployeeID { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
---
+++ Hotel.UseCases/Employees/Queries/GetByIdQuery/GetEmployeeByIdQuery‎.cs
@@ -0,0 +1,11 @@
+﻿using Application.DTOS.Employees.Response;
+using Hotel.UseCases.Commons.Bases;
+using MediatR;
+
+namespace Hotel.UseCases.Employees.Queries.GetByIdQuery
+{
+    public class GetEmployeeByIdQuery : IRequest<BaseResponse<GetEmployeeByIdResponseDto>>
+    {
+        public int EmployeeID { get; set; }
+    }
+}
