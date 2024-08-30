179a2df4d2b751d8d3f8db601391069be16028cf
Jasson Rolando Cu <jassoncucaal14@hotmail.com>
2024-08-30 12:32:14 -0600

    feat: :art: Add `Mapping for Dtos`

---
+++ Hotel.UseCases/Mapping/EmployeeMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using Application.DTOS.Employees.Response;
+using AutoMapper;
+using Hotel.Domian.Entities;
+
+namespace Hotel.UseCases.Mapping
+{
+    public class EmployeeMappingProfile : Profile
+    {
+        public EmployeeMappingProfile() {
+
+            CreateMap<Employee, GetEmployeesResponseDto>()
+                .ReverseMap();
+
+            CreateMap<Employee, GetEmployeeByIdResponseDto>()
+                .ReverseMap();
+        }
+    }
+}
