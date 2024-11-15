630499405c6139f51383158ac21064b952de1dc7
Jasson Rolando Cu <jassoncucaal14@hotmail.com>
2024-11-14 18:35:49 -0600

    refactor: :fire: Detele unnecesary elements

--- Hotel.Api/Extensions/Middleware/MiddlewareExtensions‎.cs
+++
@@ -1,9 +0,0 @@
-namespace Hotel.Api.Extensions.Middleware;
-
-public static class MiddlewareExtensions
-{
-    public static IApplicationBuilder AddMiddleware(this IApplicationBuilder builder)
-    {
-        return builder.UseMiddleware<ValidationMiddleware>();
-    }
-}
--- Hotel.Api/Program.cs
+++ Hotel.Api/Program.cs
@@ -1,27 +1,9 @@
-using Hotel.Persistence.Context;
-using Hotel.Persistence.Extensions;
-using Hotel.UseCases.Extensions;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
-builder.Services.AddInyectionPersistence();
-builder.Services.AddInyectionApplication();
-
-/*
-The database used in this project is a database that was made
-from a docker container to facilitate its development.
-When development is finished and the latest update is uploaded, the container will be deleted.
-*/
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("HotelConnectionString"));
-});
-
 
 builder.Services.AddControllers();
 
